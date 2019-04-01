60041fd2f98dd1f4fbd1de69d341dffc8a9492fc
Steven Fischbach <steven.fischbach@gmail.com>
2019-04-01 12:03:04 -0700

    added function to transform list of dataframes all at once

--- full_script.py
+++ full_script.py
@@ -59,29 +59,34 @@ df4 = pd.read_csv('/home/superstinky/Seattle_g89/final_project_data/flight_engin
 #### add column to the end for logistic predictive model   ######
 #### 
 
+data_frames_to_transform = [df1, df2, df3 , df4]
 
-dataf = df1
-max_cycles = []
-y_failure = []
-for num in range(1, max(dataf['unit']) + 1):
-#   print(num)
-    max_cycles.append(max(dataf['time_cycles'][dataf['unit']==num] ) )
-#   len(max_cycles)
-    cycles_to_fail = []
-    for total in max_cycles:
-        for cycle in range(total, 0, -1):
-            y_failure.append( 1-(cycle/total) )
-            cycles_to_fail.append(cycle)
-    # print(cycles_to_fail)
-    # len(cycles_to_fail)
-    # len(df1)
-        
-dataf['cycles_to_fail'] = cycles_to_fail
+def transform_dataframes_add_ys(data_list= [ ] , *args ):
+# dataf = df1
+    for df in data_list:
+        max_cycles = []
+        y_failure = []
+        for num in range(1, max(df['unit']) + 1):
+            #print(num)
+            max_cycles.append(max(df['time_cycles'][df['unit']==num] ) )
+            # max_cycles
+        cycles_to_fail = []
+        for total in max_cycles:
+            for cycle in range(total, 0, -1):
+                y_failure.append( 1-(cycle/total) )
+                cycles_to_fail.append(cycle)
+        # print(cycles_to_fail)
+        len(cycles_to_fail)
+        len(df)
+        len(y_failure)            
+        df['cycles_to_fail'] = cycles_to_fail
+        df['y_failure'] = y_failure
 
-
-df1 = dataf
 # df1.cycles_to_fail
 
+###   Transform all four dataframes   #######
+
+transform_dataframes_add_ys(data_frames_to_transform)
 ############################
 
 
