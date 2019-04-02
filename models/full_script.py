cf98414856ff629be865a968fdbc37261376d1b6
Steven Fischbach <steven.fischbach@gmail.com>
2019-04-02 08:28:03 -0700

    fixed the plotting problem on the predictions for pipline

--- models/full_script.py
+++ models/full_script.py
@@ -212,6 +212,7 @@ for num in range(1,101):
         train_engines.append(num)
         #
 
+
 train_engines = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 22, 23, 24, 25, 26, 
     27, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 43, 44, 47, 48, 49, 51, 52, 53, 54, 55, 
     56, 57, 58, 59, 60, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 75, 76, 77, 79, 80, 81, 85, 
@@ -548,9 +549,10 @@ train_eng_max_cycles
 
 
 #### Third plot that will show the difference from actuals vs pred for the pipeline model for each engine  ###### 
+start_idx = 0
 for idx, e in enumerate(train_engines):
-    start_idx = 0
     end_idx = start_idx + train_eng_max_cycles[idx]
+    print(start_idx, end_idx, train_eng_max_cycles[idx], end_idx-start_idx)
     fig, ax = plt.subplots(figsize=(15,15) )
     ax.plot(list(range(start_idx, end_idx)) , y_hat[start_idx:end_idx], '.r', label='predicted')
     ax.plot(list(range(start_idx, end_idx)) , ytrain[start_idx:end_idx] , '.b' , label='actual')
