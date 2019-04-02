667e94e0f1e3b3471e0772284aadc99bc4929d95
Steven Fischbach <steven.fischbach@gmail.com>
2019-04-02 12:46:18 -0700

    added new files for logistic and for liner spline. Full script is going to be just the import, data wrangling, and analysis.

--- models/full_script_linear_spline.py
+++ models/full_script_linear_spline.py
@@ -614,3 +614,22 @@ first_knot_model
 # first_knot_model
 # 0.64194677350961
 
+
+### This will function will create the actual estimations vs predicted values
+def plot_many_predicteds_vs_actuals(var_names, y_hat, n_bins=50):
+    fig, axs = plt.subplots(len(var_names), figsize=(12, 3*len(var_names)))
+    for ax, name in zip(axs, var_names):
+        x = df_new_train[name]
+        predicteds_vs_actuals(ax, x, df_new_train["cycles_to_fail"], y_hat, n_bins=n_bins)
+        # ax.set_title("{} Predicteds vs. Actuals".format(name))
+    return fig, axs
+
+
+
+### This will plot the final estimations vs the actual data
+train_features
+# y_hat = model.predict(features.values)
+fig, axs = plot_many_predicteds_vs_actuals(train_features, y_hat)
+# fig.tight_layout()df1
+plt.show()
+
